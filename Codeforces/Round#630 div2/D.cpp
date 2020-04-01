a6e18f8703459f98936203becbc1aa1279f0a9aa
Culaccino <49057685+BBBoundary@users.noreply.github.com>
2020-04-01 16:45:32 +0800

    Add files via upload

---
+++ Codeforces/Round#630 div2/D.cpp
@@ -0,0 +1,32 @@
+#include<cstdio>
+#include<iostream>
+#include<algorithm>
+#include<unordered_map>
+#include<map>
+#include<set>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<string>
+#include<cstring>
+#include<cmath>
+#include<limits>
+#include<functional>
+//#define INF 0x3f3f3f3f
+#define pb push_back
+using namespace std;
+typedef long long ll;
+typedef unsigned long long llu;
+typedef pair<int,int> pii;
+
+const int INF = 1<<17;
+
+int main()
+{
+    int k;
+    scanf("%d",&k);
+    printf("%d %d\n",2,3);
+    printf("%d %d %d\n",INF+k,k,0);
+    printf("%d %d %d\n",INF,INF+k,k);
+}
+
