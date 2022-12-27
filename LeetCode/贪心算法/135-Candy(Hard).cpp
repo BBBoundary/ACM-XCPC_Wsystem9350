f765b1777e5f3a554e22dffddf964bd84fc1d43f
Culaccino <49057685+BBBoundary@users.noreply.github.com>
2022-12-27 22:32:18 +0800

    Add files via upload

--- LeetCode/贪心算法/135-Candy(Hard).cpp
+++ LeetCode/贪心算法/135-Candy(Hard).cpp
@@ -3,6 +3,12 @@ public:
     int candy(vector<int>& ratings) {
         int i=0,cnt[20010];
         int sum=0;
+        /*
+         三次遍历
+         第一次：先将每一位的基础值置为1
+         第二次：从左到右，若当前ratings[i]>ratings[i-1],则cnt[i]=cnt[i-1]+1;
+         第三次：从右到左，若当前ratings[i]>ratings[i+1],则cnt[i]=cnt[i+1]+1;
+         */
         while(i<ratings.size()) {cnt[i]=1;i++;}
         i=1;
         while(i<ratings.size()){
